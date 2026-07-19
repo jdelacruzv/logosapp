83445e1d6dc61da21ee42187fd5b72177a002824
Jose De La Cruz <jldlcv@gmail.com>
2026-07-19 15:19:55 +0200

    Decouple Header component using application events

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -6,3 +6,20 @@ export function onLogosInit(callback: () => void): void {
   callback();
   document.addEventListener("logos:init", callback);
 }
+
+/**
+ * Emitir un evento personalizado.
+ */
+export function emit(eventName: string): void {
+	document.dispatchEvent(new Event(eventName));
+}
+
+/**
+ * Escuchar un evento personalizado.
+ */
+export function on(
+	eventName: string,
+	callback: EventListener,
+): void {
+	document.addEventListener(eventName, callback);
+}
