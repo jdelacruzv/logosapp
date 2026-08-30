866839a5679c32380eb4b098e6bbe47465e7faeb
Jose De La Cruz <jldlcv@gmail.com>
2026-08-30 18:13:00 +0200

    Replace node adapter with vercel adapter for SSR deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,11 @@
-// // @ts-check
-// import { defineConfig } from 'astro/config';
-
-// import tailwindcss from '@tailwindcss/vite';
-
-// // https://astro.build/config
-// export default defineConfig({
-//   vite: {
-//     plugins: [tailwindcss()]
-//   }
-// });
-
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node'; 
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   output: 'server', // <--- 2. Cambia a modo servidor
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
